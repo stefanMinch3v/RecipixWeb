10f5b9b81c2a53e976661db216f9c09d658e8ebd
stefan <amxx@abv.bg>
2019-01-09 11:15:01 +0100

    footer design fixed

--- src/client/app/components/shared/footer/footer.component.ts
+++ src/client/app/components/shared/footer/footer.component.ts
@@ -2,7 +2,8 @@
 
 @Component({
     selector: 'app-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styles: ['.footer { text-align: center; color: #D5648F }']
 })
 export class FooterComponent {
     constructor() { }
